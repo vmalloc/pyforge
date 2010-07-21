9cbd01335d0335d4ce25a47d7f590ea77a44b2ef
Rotem Yaari <vmalloc@gmail.com>
2010-07-21 23:25:33 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name="forge",
+      description="Python mocking framework",
+      author="Rotem Yaari",
+      author_email="vmalloc@gmail.com",
+      version="0.1",
+      packages=["forge"])
