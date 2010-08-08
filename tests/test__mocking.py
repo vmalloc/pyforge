59975bec437bbbef0d3834d5ed7c8262150d1db6
Rotem Yaari <vmalloc@gmail.com>
2010-08-08 23:41:29 +0300

    improvements to tests

--- tests/test__mocking.py
+++ tests/test__mocking.py
@@ -1,15 +1,10 @@
 from ut_utils import ForgeTestCase
 from forge.mock_handle import MockHandle
-from forge import UnexpectedCall, UnauthorizedMemberAccess, SignatureException
+from forge import UnauthorizedMemberAccess
 
 class MockedClass(object):
-    def method_without_self():
+    def some_method(self):
         raise NotImplementedError()
-    def regular_method(self, a, b, c=2, *args, **kwargs):
-        raise NotImplementedError()
-    def regular_method_without_args(self):
-        raise NotImplementedError()
-
 class MockingTest(ForgeTestCase):
     def setUp(self):
         super(MockingTest, self).setUp()
@@ -39,33 +34,6 @@ class MockingTest(ForgeTestCase):
     def test__getattr_of_methods_during_replay(self):
         self.forge.replay()
         with self.assertRaises(UnauthorizedMemberAccess):
-            self.obj.regular_method
+            self.obj.some_method
 
-    def test__record_replay_regular_methods(self):
-        self.obj.regular_method(1, 2, 3)
-        self.forge.replay()
-        self.obj.regular_method(1, 2, 3)
-        self.forge.verify()
-    def test__record_method_invalid_args(self):
-        with self.assertRaises(SignatureException):
-            self.obj.regular_method()
-        with self.assertRaises(SignatureException):
-            self.obj.regular_method_without_args(1)
-    def test__record_method_invalid_method(self):
-        with self.assertRaises(SignatureException):
-            self.obj.method_without_self()
-    def test__record_replay_missing_record(self):
-        self.forge.replay()
-        with self.assertRaises(UnauthorizedMemberAccess) as caught:
-            self.obj.regular_method
-        exc = caught.exception
-        self.assertIs(exc.object, self.obj)
-        self.assertEquals(exc.attribute, "regular_method")
-    def test__record_replay_unexpected_call(self):
-        self.obj.regular_method(1, 2, 3)
-        self.forge.replay()
-        with self.assertRaises(UnexpectedCall) as caught:
-            self.obj.regular_method(1, 2, 3, 4)
-        self.assertIs(caught.exception.expected.target, self.obj.regular_method)
-        self.assertIs(caught.exception.got.target, self.obj.regular_method)
         
