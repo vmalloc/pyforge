f9256d835d0c49ddb8a8fce89b3dc037a9f06719
Rotem Yaari <vmalloc@gmail.com>
2010-08-09 23:00:01 +0300

    Enable stubbing module-level functions

--- forge/stub_manager.py
+++ forge/stub_manager.py
@@ -1,23 +1,42 @@
+import types
+
 class StubManager(object):
     def __init__(self, forge):
         super(StubManager, self).__init__()
         self.forge = forge
         self._stubs = []
     def replace_with_stub(self, obj, method_name):
-        returned = self.forge.create_method_stub(getattr(obj, method_name), obj)
-        setattr(obj, method_name, returned)
-        self._stubs.append(InstalledStub(obj, method_name, getattr(obj, method_name)))
-        return returned
+        if isinstance(obj, types.ModuleType):
+            return self._replace_module_function_with_stub(obj, method_name)
+        return self._replace_object_method_with_stub(obj, method_name)
+
+    def _replace_object_method_with_stub(self, obj, method_name):
+        return self._replace_with_stub(obj, method_name,
+                                       self.forge.create_method_stub(getattr(obj, method_name), obj))
+    def _replace_module_function_with_stub(self, module, function_name):
+        return self._replace_with_stub(module, function_name,
+                                       self.forge.create_function_stub(getattr(module, function_name)))
+    def _replace_with_stub(self, obj, attr_name, stub):
+        orig = getattr(obj, attr_name)
+        setattr(obj, attr_name, stub)
+        self._stubs.append(InstalledStub(obj, attr_name, orig, stub))
+        return stub
     def restore_all_stubs(self):
         while self._stubs:
             installed = self._stubs.pop(-1)
-            installed.restore()            
+            installed.restore()
 
 class InstalledStub(object):
-    def __init__(self, obj, method_name, orig):
+    def __init__(self, obj, method_name, orig, stub):
         super(InstalledStub, self).__init__()
         self.obj = obj
         self.method_name = method_name
         self.orig = orig
+        self.stub = stub
     def restore(self):
-        delattr(self.obj, self.method_name)
+        if self._is_bound_method():            
+            delattr(self.obj, self.method_name)
+        else:
+            setattr(self.obj, self.method_name, self.orig)
+    def _is_bound_method(self):
+        return type(self.orig) is types.MethodType and self.orig.im_self is not None
--- tests/test__stubbing.py
+++ tests/test__stubbing.py
@@ -1,7 +1,9 @@
 from ut_utils import ForgeTestCase
 from forge.stub import FunctionStub
 import time
+orig_time_sleep = time.sleep
 import os
+orig_os_path_join = os.path.join
 
 class NewStyleClass(object):
     def method(self, a, b, c):
@@ -23,12 +25,32 @@ class StubbingObjectsTest(ForgeTestCase):
         self.assertIs(obj.method._signature.func.im_func, expected.im_func)
         self.assertTrue(obj.method._signature.is_first_argument_self())
         self.forge.restore_all_stubs()
-        self.assertIs(obj.method.im_func, expected.im_func)        
+        self.assertIs(obj.method.im_func, expected.im_func)
     def test__stubbing_new_style_objects(self):
         self._test__stubbing_object(NewStyleClass(), 'method', orig_newstyle_method)
     def test__stubbing_old_style_objects(self):
         self._test__stubbing_object(OldStyleClass(), 'method', orig_oldstyle_method)
-        
 
 
-
+class StubbingModulesTest(ForgeTestCase):
+    def test__stub_c_function(self):
+        self.forge.replace_with_stub(time, "sleep")
+        self.assertIsInstance(time.sleep, FunctionStub)
+        expected_result = 666
+        time.sleep(10).and_return(expected_result)
+        self.forge.replay()
+        self.assertEquals(time.sleep(10), expected_result)
+        self.forge.restore_all_stubs()
+        self.assertIs(time.sleep, orig_time_sleep)
+    def test__stub_module_functions(self):
+        self.forge.replace_with_stub(os.path, "join")
+        self.assertIsInstance(os.path.join, FunctionStub)
+        self.assertFalse(os.path.join._signature.has_variable_kwargs())
+        self.assertTrue(os.path.join._signature.has_variable_args())
+        return_path = "return_path"
+        os.path.join("a", "b", "c").and_return(return_path)
+        self.forge.replay()
+        self.assertEquals(return_path, os.path.join("a", "b", "c"))
+        self.forge.verify()
+        self.forge.restore_all_stubs()
+        self.assertIs(os.path.join, orig_os_path_join)
