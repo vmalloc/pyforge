b2f09fbea4aa9e1268e015dc7bbc004687c01e03
Rotem Yaari <vmalloc@gmail.com>
2010-08-26 15:32:48 +0300

    tests for __len__ and __setitem__

--- tests/test__special_methods.py
+++ tests/test__special_methods.py
@@ -1,6 +1,7 @@
 from ut_utils import ForgeTestCase
 from ut_utils import build_new_style_class
 from ut_utils import build_old_style_class
+from forge import UnexpectedCall
 
 class _SpecialMethodsTest(ForgeTestCase):
     def setUp(self):
@@ -16,6 +17,19 @@ class _SpecialMethodsTest(ForgeTestCase):
         self.obj.__len__().AndReturn(2)
         self.forge.replay()
         self.assertEquals(len(self.obj), 2)
+    def test__setitem_explicit(self):
+        self.obj.__setitem__('a', 'b')
+        self.forge.replay()
+        self.obj['a'] = 'b'
+    def test__setitem_implicit(self):
+        self.obj['a'] = 'b'
+        self.forge.replay()
+        self.obj['a'] = 'b'
+    def test__setitem_mismatch(self):
+        self.obj['a'] = 'b'
+        self.forge.replay()
+        with self.assertRaises(UnexpectedCall):
+            self.obj['a'] = 'c'
 
 class NewStyleSpecialMethodsTest(_SpecialMethodsTest):
     CTOR = staticmethod(build_new_style_class)
