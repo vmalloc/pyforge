ea1ceb3ccf21fd017ab47ef6c141c4f5be0d7e10
Rotem Yaari <vmalloc@gmail.com>
2010-09-09 16:36:46 +0300

    Rename stub manager to stub installer

--- forge/forge.py
+++ forge/forge.py
@@ -4,7 +4,7 @@ from .stub import FunctionStub
 from .queue import ForgeQueue
 from .instance_mock_object import InstanceMockObject
 from .wildcard_mock_object import WildcardMockObject
-from .stub_manager import StubManager
+from .stub_installer import StubInstaller
 from .mock_method_manager import MockMethodManager
 from .sentinel import Sentinel
 from .dtypes import WILDCARD_FUNCTION
@@ -12,7 +12,7 @@ from .dtypes import WILDCARD_FUNCTION
 class Forge(object):
     def __init__(self):
         super(Forge, self).__init__()
-        self.stubs = StubManager(self)
+        self.stub_installer = StubInstaller(self)
         self.reset()
         self._id_allocator = itertools.count()
     def get_new_handle_id(self):
@@ -51,8 +51,8 @@ class Forge(object):
     def create_sentinel(self, name=None):
         return Sentinel(name)
     def replace_with_stub(self, obj, method_name):
-        return self.stubs.replace_with_stub(obj, method_name)
+        return self.stub_installer.replace_with_stub(obj, method_name)
     def restore_all_stubs(self):
-        self.stubs.restore_all_stubs()
+        self.stub_installer.restore_all_stubs()
     def any_order(self):
         return self.queue.get_unordered_group_context()
--- forge/stub_installer.py
+++ forge/stub_installer.py
@@ -1,8 +1,8 @@
 import types
 
-class StubManager(object):
+class StubInstaller(object):
     def __init__(self, forge):
-        super(StubManager, self).__init__()
+        super(StubInstaller, self).__init__()
         self.forge = forge
         self._stubs = []
     def replace_with_stub(self, obj, method_name):
