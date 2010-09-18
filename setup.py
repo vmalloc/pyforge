55cb8c09721cdd0a9164f8ddf3ae5a0c11c4e4a2
Rotem Yaari <vmalloc@gmail.com>
2010-09-19 00:02:00 +0200

    Fix setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from distutils.core import setup
 setup(name="forge",
+      classifiers = [
+          "Programming Language :: Python :: 2",
+          "Intended Audience :: Developers",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       description="Python mocking framework",
+      license="BSD",
       author="Rotem Yaari",
       author_email="vmalloc@gmail.com",
+      url="http://github.com/vmalloc/forge",
       version="0.1",
-      packages=["forge"])
+      packages=["forge"]
+      )
