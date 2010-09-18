3247b77b82edc771345659362e86cab8e8fe8cf9
Rotem Yaari <vmalloc@gmail.com>
2010-09-19 00:15:40 +0200

    Change official name to 'pyforge'

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
-setup(name="forge",
+setup(name="pyforge",
       classifiers = [
-          "Programming Language :: Python :: 2",
+          "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python :: 2.6",
           "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Software Development :: Testing",
           ],
       description="Python mocking framework",
       license="BSD",
