8352ed7ad68c7b86807357196a2be76dbebee701
Alon Horev <alonho@gmail.com>
2010-12-05 11:53:40 +0200

    add EXPECTING to utils as a shortcut for setattr expectations

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -1,7 +1,17 @@
-from ut_utils import TestCase
-from forge.utils import renumerate
+from ut_utils import TestCase, ForgeTestCase
+from forge.utils import renumerate, EXPECTING
+from forge.class_mock_handle import ClassMockHandle
 
 class RenumerateTest(TestCase):
     def test__simple_usage(self):
         self.assertEquals(list(renumerate(range(5))),
                           [(4, 4), (3, 3), (2, 2), (1, 1), (0, 0)])
+
+class EXPECTING_Test(ForgeTestCase):
+    def test(self):
+        mocked = self.forge.create_wildcard_mock()
+        handle = self.forge.create_mock(ClassMockHandle)
+        mocked.__forge__ = handle
+        handle.expect_setattr("foo", "bar")
+        with self.forge.verified_replay_context():
+            EXPECTING(mocked).foo = "bar"
