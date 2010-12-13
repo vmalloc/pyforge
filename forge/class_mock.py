9830bf090d8c828d06292cc852150d1cf6c65b11
Sagiv Malihi <sagivmalihi@gmail.com>
2010-12-13 11:30:40 +0200

    a somehwhat ugly patch to ClassMockObject to imitate the mocked class's mro

--- forge/class_mock.py
+++ forge/class_mock.py
@@ -1,12 +1,21 @@
 from class_mock_handle import ClassMockHandle
 from mock_object import MockObject
 
-class ClassMockObject(MockObject):
+class _ClassMockObject(MockObject):
     def __init__(self, forge, mocked_class, behave_as_instance, hybrid):
-        super(ClassMockObject, self).__init__()
+        super(_ClassMockObject, self).__init__()
         self.__forge__ = ClassMockHandle(forge, self, mocked_class, behave_as_instance, hybrid)
     @property
     def __class__(self):
         if self.__forge__.behaves_as_instance:
             return self.__forge__.mocked_class
         return type(self.__forge__.mocked_class)
+    @classmethod
+    def mro(cls):
+        return cls.mocked_class.mro()
+
+
+def ClassMockObject(forge, mocked_class, behave_as_instance, hybrid):
+    class tmpClass(_ClassMockObject): pass
+    tmpClass.mocked_class = mocked_class
+    return tmpClass(forge, mocked_class, behave_as_instance, hybrid)
