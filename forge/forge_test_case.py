0033dd7a1ce77a0293d72fd2276ce9a90d5a61a5
Rotem Yaari <vmalloc@gmail.com>
2011-07-27 13:21:48 +0300

    v0.5.100: add ForgeTestCase to pyforge namespace

---
+++ forge/forge_test_case.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from .forge import Forge
+
+class ForgeTestCase(TestCase):
+    def setUp(self):
+        super(ForgeTestCase, self).setUp()
+        self.forge = Forge()
+    def tearDown(self):
+        try:
+            self.forge.verify()
+        finally:
+            self.forge.restore_all_replacements()
+        super(ForgeTestCase, self).tearDown()
