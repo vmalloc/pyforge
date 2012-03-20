095e1366bc4dbd5ee6d979833d46da50426e9ab6
Rotem Yaari <vmalloc@gmail.com>
2012-03-20 11:40:09 +0200

    Fix installation for python 2.6

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import os
+import platform
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), "forge", "__version__.py")) as version_file:
     exec(version_file.read())
+
+_INSTALL_REQUIREMENTS = ['sentinels>=0.0.4']
+if platform.python_version() < '2.7':
+    _INSTALL_REQUIREMENTS.append('unittest2')
 
 setup(name="pyforge",
       classifiers = [
@@ -19,6 +24,6 @@ setup(name="pyforge",
       author_email="vmalloc@gmail.com",
       url="http://github.com/vmalloc/pyforge",
       version=__version__,
-      install_requires=['sentinels>=0.0.4'],
+      install_requires=_INSTALL_REQUIREMENTS,
       packages=find_packages(exclude=["tests"])
       )
