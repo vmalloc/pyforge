85357b494c8b63f0666c5c990cd2f2fc8b40d919
Ayala Shachar <shachar.ayala@gmail.com>
2020-10-04 19:04:55 +0300

    Switch to PBR

--- setup.py
+++ setup.py
@@ -1,29 +1,10 @@
-import os
-import platform
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "forge", "__version__.py")) as version_file:
-    exec(version_file.read())
 
-_INSTALL_REQUIREMENTS = ['sentinels>=0.0.4']
-
-setup(name="pyforge",
-      classifiers = [
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: BSD License",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "Topic :: Software Development :: Testing",
-          ],
-      description="Python mocking framework",
-      license="BSD",
-      author="Rotem Yaari",
-      author_email="vmalloc@gmail.com",
-      url="http://github.com/vmalloc/pyforge",
-      version=__version__,
-      install_requires=_INSTALL_REQUIREMENTS,
-      packages=find_packages(exclude=["tests"])
-      )
+setup(
+    setup_requires=['pbr>=3.0', 'setuptools>=17.1'],
+    pbr=True,
+    python_requires=">=3.6.*",
+    long_description_content_type='text/markdown; charset=UTF-8',
+)
