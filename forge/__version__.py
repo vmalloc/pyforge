b4dc78ef31ff55392b0447fa88d6450f08f0a841
Ayala Shachar <shachar.ayala@gmail.com>
2024-09-27 17:50:34 +0300

    Remove deprecated package: pkg_resources

--- forge/__version__.py
+++ forge/__version__.py
@@ -1,3 +1,9 @@
-import pkg_resources
+import sys
+if sys.version_info < (3, 8):
+   import pkg_resources
+   get_distribution = pkg_resources.get_distribution
+else:
+   from importlib.metadata import distribution
+   get_distribution = distribution
 
-__version__ = pkg_resources.get_distribution('pyforge').version
+__version__ = get_distribution('pyforge').version
